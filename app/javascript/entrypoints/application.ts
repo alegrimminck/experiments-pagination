576d45a40cbf389a108179b58f5baa9a865450a0
Alejandro Grimminck <alegrimminck@gmail.com>
2026-01-31 10:41:11 -0300

    feat(router): add vue router

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from '../pages/App.vue'
+import { router } from '../router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(VueQueryPlugin)
+app.mount('#app')
